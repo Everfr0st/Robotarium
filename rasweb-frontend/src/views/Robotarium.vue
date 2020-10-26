e7e224bd2ef0dd7d49d13c9bd313d3133d3bb9fc
Felipe Cerquera <andrescercal@hotmail.com>
2020-10-26 10:12:18 -0500

    Working in VideoMainStream.vue and added shaka player .js library

--- rasweb-frontend/src/views/Robotarium.vue
+++ rasweb-frontend/src/views/Robotarium.vue
@@ -1,5 +1,18 @@
 <template>
     <div>
         <h2>RObotarium</h2>
+        <MainVideoStream />
     </div>
 </template>
+
+
+
+<script>
+import MainVideoStream from "@/components/MainVideoStream.vue";
+export default {
+    name: "Robotarium",
+    components: {
+        MainVideoStream
+    }
+}
+</script>

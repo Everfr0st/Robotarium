4a5aba04a56fb0f9967cfe84729f5b469ad3bee0
Felipe Cerquera <andrescercal@hotmail.com>
2020-12-22 10:15:01 -0500

    Some changes

--- rasweb-frontend/src/views/Inventory.vue
+++ rasweb-frontend/src/views/Inventory.vue
@@ -2,16 +2,19 @@
     <div>
         <Stand class="pa-0" stand="A" levels="5" />
         <Stand class="mt-4 pa-0" stand="B" levels="6" />
-
+<Reservation/>
     </div>
 </template>
 
 <script>
 import Stand from "@/components/Stand.vue";
+import Reservation from "@/components/Reservation.vue"
 import {mapMutations} from "vuex";
+import Reservation from '../components/Reservation.vue';
 export default {
     name: "Inventory",
     components:{
+        Reservation,
         Stand
     },
     created(){

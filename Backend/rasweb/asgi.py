e4057d7486cf9f9e465f6ddb227816edefc7bc95
Felipe Cerquera <andrescercal@hotmail.com>
2021-02-25 11:27:40 -0500

    Asgi handler modified

--- Backend/rasweb/asgi.py
+++ Backend/rasweb/asgi.py
@@ -1,18 +1,33 @@
-"""
-ASGI config for rasweb project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
-"""
-
 import os
 
+from django.conf.urls import url
 from django.core.asgi import get_asgi_application
-import django
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rasweb.settings')
-django.setup()
-application = get_asgi_application()
+# Fetch Django ASGI application early to ensure AppRegistry is populated
+# before importing consumers and AuthMiddlewareStack that may import ORM
+# models.
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rasweb.settings")
+django_asgi_app = get_asgi_application()
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+
+from Apps.Chat import routing as chat_routing
+from Apps.LiveStream import routing as live_routing
+from Apps.Notifications import routing as notify_routing
+from Apps.Robotarium import routing as robotarium_routing
+
+application = ProtocolTypeRouter({
+    # Django's ASGI application to handle traditional HTTP requests
+    "http": django_asgi_app,
+
+    # WebSocket chat handler
+    "websocket": AuthMiddlewareStack(
+        URLRouter([
+            chat_routing.websocket_urlpatterns +
+            live_routing.websocket_urlpatterns +
+            notify_routing.websocket_urlpatterns +
+            robotarium_routing.websocket_urlpatterns
+        ])
+    ),
+})
